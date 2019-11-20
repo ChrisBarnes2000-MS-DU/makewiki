089b5519b1721d1fa7864f72667edb8f5d98d248
Christopher Barnes <cbarnes7404@yahoo.com>
2019-11-20 15:17:03 -0800

    add edit page and test

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from wiki.views import PageListView, PageDetailView, PageCreateView
-from wiki.forms import PageForm
+from wiki.views import PageListView, PageDetailView, PageCreateView, PageEditView
 
 urlpatterns = [
     path('', PageListView.as_view(), name='wiki-list-page'),
     path('new/', PageCreateView.as_view(), name='wiki-create-page'),
     path('<str:slug>/', PageDetailView.as_view(), name='wiki-details-page'),
-    
+    path('<str:slug>/edit/', PageEditView.as_view(), name='wiki-edit-page'),
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -1,10 +1,10 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView
 from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
 from django.urls import reverse_lazy
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from wiki.forms import PageForm
@@ -32,11 +32,7 @@ class PageDetailView(DetailView):
         page = self.get_queryset().get(slug__iexact=slug)
         return render(request, 'page.html', {'page': page})
 
-class PageCreateView(FormView):
-    template_name = 'create.html'
-    from_class = PageForm
-    success_url = '/'
-
+class PageCreateView(CreateView):
     def get(self, request, *args, **kwargs):
         context = {'form': PageForm()}
         return render(request, 'create.html', context)
@@ -49,4 +45,21 @@ class PageCreateView(FormView):
             page.published_date = timezone.now()
             page.save()
             return HttpResponseRedirect(reverse_lazy('wiki-details-page', args=[page.slug]))
+        return render(request, 'create.html', {'form': form})
+
+class PageEditView(CreateView):
+    model = Page
+
+    def get(self, request, slug):
+        page = get_object_or_404(Page, slug=slug)
+        if request.method == "POST":
+            form = PageForm(request.POST, instance=page)
+            if form.is_valid():
+                page = form.save(commit=False)
+                page.author = request.user
+                page.published_date = timezone.now()
+                page.save()
+                return redirect('wiki-details-page', slug=page.slug)
+        else:
+            form = PageForm(instance=page)
         return render(request, 'create.html', {'form': form})
