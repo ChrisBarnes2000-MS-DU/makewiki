176d359d4bdca22d971ab13982894748c1150d95
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-12 10:28:36 -0800

    update staticfiles settings

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1yct-t!2bnkgc7j59z+9cdd2k)@y+ftqor$!aya()3if^cnlo-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'makewiki-cb.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'makewiki-cb.herokuapp.com', '127.0.0.1']
 
 DEFAULT_LOGOUT_URL = '/'
 
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
