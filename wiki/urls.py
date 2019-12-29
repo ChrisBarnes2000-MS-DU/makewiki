1b87eef670fca4f0693a29da0944af660cdf1a40
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-28 23:59:04 -0800

    refractor views & templates, fixed edit and delete

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from wiki.views import PageListView, PageDetailView, PageCreateView, PageEditView
+from wiki.views import PageListView, PageDetailView, PageCreateView, PageEditView, PageDeleteView
 
 urlpatterns = [
     path('', PageListView.as_view(), name='wiki-list-page'),
     path('new/', PageCreateView.as_view(), name='wiki-create-page'),
     path('<str:slug>/', PageDetailView.as_view(), name='wiki-details-page'),
     path('<str:slug>/edit/', PageEditView.as_view(), name='wiki-edit-page'),
+    path('<str:slug>/delete/', PageDeleteView.as_view(), name='wiki-delete-page'),
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -1,6 +1,4 @@
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
-from django.views.generic.edit import FormView, CreateView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
 from django.urls import reverse_lazy
@@ -15,51 +13,75 @@ def logout_view(request):
     # Redirect to a success page.
 
 class PageListView(ListView):
-    """ Renders a list of all Pages. """
-    model = Page
+    template_name = 'wiki/list.html'
+    context_object_name = 'pages'
 
-    def get(self, request):
-        """ GET a list of Pages. """
-        pages = self.get_queryset().all()
-        return render(request, 'list.html', {'pages': pages})
+    def get_queryset(self):
+        return Page.objects.all()
 
 class PageDetailView(DetailView):
-    """ Renders a specific page based on it's slug."""
     model = Page
-
-    def get(self, request, slug):
-        """ Returns a specific wiki page by slug. """
-        page = self.get_queryset().get(slug__iexact=slug)
-        return render(request, 'page.html', {'page': page})
+    template_name = 'wiki/page.html'
 
 class PageCreateView(CreateView):
-    def get(self, request, *args, **kwargs):
-        context = {'form': PageForm()}
-        return render(request, 'create.html', context)
-
-    def post(self, request, *args, **kwargs):
-        form = PageForm(request.POST)
-        if form.is_valid():
-            page = form.save(commit=False)
-            page.author = request.user
-            page.published_date = timezone.now()
-            page.save()
-            return HttpResponseRedirect(reverse_lazy('wiki-details-page', args=[page.slug]))
-        return render(request, 'create.html', {'form': form})
-
-class PageEditView(CreateView):
     model = Page
+    fields = ['title', 'content']
 
-    def get(self, request, slug):
-        page = get_object_or_404(Page, slug=slug)
-        if request.method == "POST":
-            form = PageForm(request.POST, instance=page)
-            if form.is_valid():
-                page = form.save(commit=False)
-                page.author = request.user
-                page.published_date = timezone.now()
-                page.save()
-                return redirect('wiki-details-page', slug=page.slug)
-        else:
-            form = PageForm(instance=page)
-        return render(request, 'create.html', {'form': form})
+class PageEditView(UpdateView):
+    model = Page
+    fields = ['title', 'content']
+
+class PageDeleteView(DeleteView):
+    model = Page
+    success_url = reverse_lazy('wiki-list-page')
+
+
+# class PageListView(ListView):
+#     """ Renders a list of all Pages. """
+#     model = Page
+
+#     def get(self, request):
+#         """ GET a list of Pages. """
+#         pages = self.get_queryset().all()
+#         return render(request, 'list.html', {'pages': pages})
+
+# class PageDetailView(DetailView):
+#     """ Renders a specific page based on it's slug."""
+#     model = Page
+
+#     def get(self, request, slug):
+#         """ Returns a specific wiki page by slug. """
+#         page = self.get_queryset().get(slug__iexact=slug)
+#         return render(request, 'page.html', {'page': page})
+
+# class PageCreateView(CreateView):
+#     def get(self, request, *args, **kwargs):
+#         context = {'form': PageForm()}
+#         return render(request, 'create.html', context)
+
+#     def post(self, request, *args, **kwargs):
+#         form = PageForm(request.POST)
+#         if form.is_valid():
+#             page = form.save(commit=False)
+#             page.author = request.user
+#             page.published_date = timezone.now()
+#             page.save()
+#             return HttpResponseRedirect(reverse_lazy('wiki-details-page', args=[page.slug]))
+#         return render(request, 'create.html', {'form': form})
+
+# class PageEditView(CreateView):
+#     model = Page
+
+#     def get(self, request, slug):
+#         page = get_object_or_404(Page, slug=slug)
+#         if request.method == "POST":
+#             form = PageForm(request.POST, instance=page)
+#             if form.is_valid():
+#                 page = form.save(commit=False)
+#                 page.author = request.user
+#                 page.published_date = timezone.now()
+#                 page.save()
+#                 return redirect('wiki-details-page', slug=page.slug)
+#         else:
+#             form = PageForm(instance=page)
+#         return render(request, 'create.html', {'form': form})
